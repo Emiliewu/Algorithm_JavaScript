a710d7787929214ff5dd5ad0714c502d8d855317
Emilie Wu <emiliewu@163.com>
2022-06-22 19:19:23 -0700

    leetcode 133 Clone Graph --dfs and bfs two way solution

--- Graph/leetcode133.js
+++ Graph/leetcode133.js
@@ -11,20 +11,37 @@
  * @return {Node}
  */
  var cloneGraph = function(node) {
-    //dfs
+   
     if(!node) return;
     const visited = new Map();
-    const dfs = (n) => {
+     //dfs
+    // const dfs = (n) => {
+    //     // console.log(n.val);
+    //     const nCopy = new Node(n.val);
+    //     visited.set(n, nCopy);
+    //    ( n.neighbors || []).forEach(ne => {
+    //         if(!visited.has(ne)) {
+    //             dfs(ne);
+    //         }
+    //         nCopy.neighbors.push(visited.get(ne));
+    //     });
+    // };
+    // dfs(node);
+
+    //bfs
+    visited.set(node, new Node(node.val));
+    const q = [node];
+    while(q.length) {
+        const n = q.shift();
         // console.log(n.val);
-        const nCopy = new Node(n.val);
-        visited.set(n, nCopy);
-       ( n.neighbors || []).forEach(ne => {
+        (n.neighbors || []).forEach(ne => {
             if(!visited.has(ne)) {
-                dfs(ne);
+                q.push(ne);
+                visited.set(ne, new Node(ne.val));
             }
-            nCopy.neighbors.push(visited.get(ne));
+            visited.get(n).neighbors.push(visited.get(ne));
         });
-    };
-    dfs(node);
+    }
+
     return visited.get(node);
 };
