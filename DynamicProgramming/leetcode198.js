7a5511c84e2b0c676200d05ded07ff020985c5af
Emilie Wu <emiliewu@163.com>
2022-07-01 10:02:35 -0700

    algorithm

--- DynamicProgramming/leetcode198.js
+++ DynamicProgramming/leetcode198.js
@@ -6,10 +6,16 @@
     if(nums.length === 0) { 
         return 0;
     }
-    const dp = [0, nums[0]];
-    for(let i = 2; i < nums.length; i++) {
-        dp[i] = Math.max((dp[i - 2] + nums[i - 1]), dp[i - 1]);
+    // const dp = [0, nums[0]];
+    let dp0 = 0;
+    let dp1 = nums[0];
+    for(let i = 2; i <= nums.length; i++) {
+        // dp[i] = Math.max((dp[i - 2] + nums[i - 1]), dp[i - 1]);
+        const dp2 = Math.max(dp0 + nums[i - 1], dp1);
+        dp0 = dp1;
+        dp1 = dp2;
     }
-    console.log(dp);
-    return dp[dp.length - 1];
+    // console.log(dp);
+    // return dp[dp.length - 1];
+    return dp1;
 };
